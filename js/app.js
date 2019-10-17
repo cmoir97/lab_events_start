452ad76eee94f5064c283e31ed258af55aff0be0
Cameron Moir <cameronjmoir@gmail.com>
2019-10-17 14:43:40 +0100

    printed new item to page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  // const inputTitle = document.querySelector('#title');
+  // inputTitle.addEventListener('input', handleInputTitle);
 })
+
+
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  const resultParagraph = document.querySelector('#reading-list');
+  // console.log('title', event.target.title.value)
+  resultParagraph.textContent = ` Title: ${event.target.title.value}
+  Author: ${event.target.author.value}
+  Category: ${event.target.category.value}`;
+};
+
+// const handleInputTitle = function (event) {
+//   const resultParagraph = document.querySelector('#input-result');
+//   resultParagraph.textContent = event.target.value;
+// }
