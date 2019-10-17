7467edf4dfddd03908da32cad1c7114c1dcf4768
Cameron Moir <cameronjmoir@gmail.com>
2019-10-17 16:11:50 +0100

    final commit

--- js/app.js
+++ js/app.js
@@ -4,22 +4,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
 
-  // const inputTitle = document.querySelector('#title');
-  // inputTitle.addEventListener('input', handleInputTitle);
 })
 
 
 
-const handleFormSubmit = function (event) {
+ handleFormSubmit = function (event) {
   event.preventDefault();
-  const resultParagraph = document.querySelector('#reading-list');
-  // console.log('title', event.target.title.value)
-  resultParagraph.textContent = ` Title: ${event.target.title.value}
-  Author: ${event.target.author.value}
-  Category: ${event.target.category.value}`;
-};
+  const resultFormSubmit = document.querySelector('#reading-list');
 
-// const handleInputTitle = function (event) {
-//   const resultParagraph = document.querySelector('#input-result');
-//   resultParagraph.textContent = event.target.value;
-// }
+const newBookList = document.createElement("ul");
+resultFormSubmit.appendChild(newBookList);
+
+const newBookTitle = document.createElement("li");
+newBookTitle.textContent = event.target.title.value;
+newBookList.appendChild(newBookTitle);
+
+const newBookAuthor = document.createElement("li");
+newBookAuthor.textContent = event.target.author.value;
+newBookList.appendChild(newBookAuthor);
+
+const newBookCategory = document.createElement("li");
+newBookCategory.textContent = event.target.category.value;
+newBookList.appendChild(newBookCategory);
+
+
+
+// const newBookAuthor = document.querySelector("li");
+// newBookAuthor.textContent = `Author: ${event.target.author.value}`
+// newBookList.appendChild(newBookAuthor);
+//
+// const newBookCategory = document.querySelector("li")
+// newBookCategory.textContent = `Category: ${event.target.author.value}`
+// newBookList.appendChild(newBookCategory);
+
+};
